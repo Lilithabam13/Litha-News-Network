168cbd1b1d7ec89aaa866697a007d2a32e2b49b4
Litha <Lilithabam1313@gmail.com>
2026-05-28 22:35:15 +0200

    Migrated LNN databse from SQLite3 to MariaDB

--- LNN/LNN/settings.py
+++ LNN/LNN/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'LNN.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lnn_db',
+        'USER': 'litha',
+        'PASSWORD': 'lithalnn01',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
